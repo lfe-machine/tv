yoshikiohshima/ex11

added asterix widget

widgets/main.erl
@@ -23,6 +23,7 @@ loop(Pid,Port,Display,Win,Ready,Graph,Num) ->
     	{event,_,expose,expose} when Ready == false -> % The window is ready for use
 			Graph1 = graph:make(Pid,Display,Win,0,0), % Place the graph
 			Num1 = numdisplay:make(Pid,Display,Win,200,20), % Place the numeric real time display
+			asterix:make(Pid,Display,Win,200,100),
 			timer:send_interval(5000, poll),
 		    ?MODULE:loop(Pid,Port,Display,Win,true,Graph1,Num1);
 		poll when is_port(Port) -> Port ! {self(), {command, "w\n"}},

lfe-machine/tv

widgets/main.erl
@@ -23,6 +23,7 @@ loop(Pid,Port,Display,Win,Ready,Graph,Num) ->
     	{event,_,expose,expose} when Ready == false -> % The window is ready for use
 			Graph1 = graph:make(Pid,Display,Win,0,0), % Place the graph
 			Num1 = numdisplay:make(Pid,Display,Win,200,20), % Place the numeric real time display
+			cartoon:make(Pid,Display,Win,200,100),
 			timer:send_interval(5000, poll),
 		    ?MODULE:loop(Pid,Port,Display,Win,true,Graph1,Num1);
 		poll when is_port(Port) -> Port ! {self(), {command, "w\n"}},